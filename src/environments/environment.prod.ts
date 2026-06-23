3ca7e4dd4ece8f01d1fb43e029dd048c5ebac1e9
Alex Carlo <alancamo2015@gmail.com>
2026-06-23 12:06:27 -0500

    hi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
+// Ambiente de producción
+
 export const environment = {
   production: true,
-  apiUrl: 'https://api.tickets-sam.com/api'
+  envName: 'production',
+  apiUrl: 'https://nest-puntual-bg-production.up.railway.app/api',
+  demoMode: false,
+  vapidPublicKey: 'BKtxfk7HNdhDBbZslqJjra8W8jaITkCfFDAsSHE-HYm5dN9IdeP6nbsDX-5w52S5AqXza_bh3FWxFnVe_hYXL4s',
+  wompiPublicKey: 'pub_prod_M2AjpgyWXuWesaGcIFoZz6vUGfttpknM',
+  wompiBaseUrl: 'https://production.wompi.co/v1',
 };
